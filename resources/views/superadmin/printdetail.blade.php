bb1be6c3d7d239769fa83d130ab9666817fb5442
Ahmad Farhan <ahmadfarhanhafidzudin555@gmail.com>
2020-02-21 11:47:47 +0700

    nambah print detail

--- resources/views/superadmin/printdetail.blade.php
+++ resources/views/superadmin/printdetail.blade.php
@@ -39,10 +39,10 @@
      <!-- Page Wrapper -->
   <div id="wrapper">
 
-    <div id="content-wrapper" class="d-flex flex-column">
+    
     
         <!-- Main Content -->
-        <div id="content">
+        
 
             <div class="container-fluid">
 
@@ -55,91 +55,89 @@
                         @endif
                         <br>
                     <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
+                    
                         <div class="card-header py-3">
                         <center><h6 class="m-0 font-weight-bold text-primary">Laporan Detail Nilai Akhir Mentee</h6>
                         <h6 class="m-0 font-weight-bold text-primary">Uji Bisnis Improvment Prajabatan Tingkat S2 PLN Group</h6>
                         </center>
                         <!-- <a href="#" class="btn btn-sm btn-primary" id="btnprint" style="right:10px;">Print</a> -->
                         </div>
-                        <div class="card-body">
-                        <div class="table-responsive">
-                <table class="table table-bordered table-sm"  width="20%" cellspacing="0">
-                  <thead>
-                    <tr>
-                    <th>No</th>
-                    <th>Nama Siswa</th>
-                    <th>Nama Penguji</th>
-                    <th>Kelas Penguji</th>
-                    <th>Nilai Sub Kategori 1 Bobot 2,5%</th>
-                    <th>Nilai Sub Kategori 2 Bobot 2,5%</th>
-                    <th>Nilai Sub Kategori 3 Bobot 10%</th>
-                    <th>Nilai Sub Kategori 4 Bobot 10%</th>
-                    <th>Nilai Sub Kategori 5 Bobot 10%</th>
-                    <th>Nilai Sub Kategori 6 Bobot 10%%</th>
-                    <th>Nilai Sub Kategori 7 Bobot 5%</th>
-                    <th>Nilai Sub Kategori 8 Bobot 10%</th>
-                    <th>Nilai Sub Kategori 9 Bobot 10%</th>
-                    <th>Nilai Sub Kategori 10 Bobot 5%</th>
-                    <th>Nilai Sub Kategori 11 Bobot 10%</th>
-                    <th>Nilai Sub Kategori 12 Bobot 15%</th>
-                    <th>Total Nilai</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  @php $no = 1; $semuanilai=0; $id=0; @endphp
-                @foreach($dataq as $datas)
-                    <tr>
-                        <td>{{$no++ }}</td>
-                        <td>{{$datas->s_nama}}</td>
-                        <td>{{$datas->p_nama}}</td>
-                        <td>{{$datas->kelas_penguji}}</td>
-                        <td>{{$datas->total_subkat_1}} </td>
-                        <td>{{$datas->total_subkat_2}} </td>
-                        <td>{{$datas->total_subkat_3}} </td>
-                        <td>{{$datas->total_subkat_4}} </td>
-                        <td>{{$datas->total_subkat_5}} </td>
-                        <td>{{$datas->total_subkat_6}} </td>
-                        <td>{{$datas->total_subkat_7}} </td>
-                        <td>{{$datas->total_subkat_8}} </td>
-                        <td>{{$datas->total_subkat_9}} </td>
-                        <td>{{$datas->total_subkat_10}} </td>
-                        <td>{{$datas->total_subkat_11}} </td>
-                        <td>{{$datas->total_subkat_12}} </td>
-                        <td>{{$datas->total_nilai_subkat}}</td>
-                    </tr>
-                      @php $semuanilai += $datas->total_nilai_subkat/3;
-                         
-                       @endphp
-                    @endforeach    
-                    <tr>
-                      <th colspan="2">
-                      Total Nilai
-                      </th>
-                      <td colspan="15">
-                      @php 
-                          echo($semuanilai);
-                       @endphp
-                       </td>
-                    </tr>                    
-                </tbody>
-                </table>
-              </div>
-                        </div>
-                    </div>
+                        
+                          <table class="table table-bordered table-sm"  width="20%" cellspacing="0">
+                            <thead>
+                              <tr>
+                              <th>No</th>
+                              <th>Nama Siswa</th>
+                              <th>Nama Penguji</th>
+                              <th>Kelas Penguji</th>
+                              <th>Nilai Sub Kategori 1 Bobot 2,5%</th>
+                              <th>Nilai Sub Kategori 2 Bobot 2,5%</th>
+                              <th>Nilai Sub Kategori 3 Bobot 10%</th>
+                              <th>Nilai Sub Kategori 4 Bobot 10%</th>
+                              <th>Nilai Sub Kategori 5 Bobot 10%</th>
+                              <th>Nilai Sub Kategori 6 Bobot 10%%</th>
+                              <th>Nilai Sub Kategori 7 Bobot 5%</th>
+                              <th>Nilai Sub Kategori 8 Bobot 10%</th>
+                              <th>Nilai Sub Kategori 9 Bobot 10%</th>
+                              <th>Nilai Sub Kategori 10 Bobot 5%</th>
+                              <th>Nilai Sub Kategori 11 Bobot 10%</th>
+                              <th>Nilai Sub Kategori 12 Bobot 15%</th>
+                              <th>Total Nilai</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                            @php $no = 1; $semuanilai=0; $id=0; @endphp
+                          @foreach($dataq as $datas)
+                              <tr>
+                                  <td>{{$no++ }}</td>
+                                  <td>{{$datas->s_nama}}</td>
+                                  <td>{{$datas->p_nama}}</td>
+                                  <td>{{$datas->kelas_penguji}}</td>
+                                  <td>{{$datas->total_subkat_1}} </td>
+                                  <td>{{$datas->total_subkat_2}} </td>
+                                  <td>{{$datas->total_subkat_3}} </td>
+                                  <td>{{$datas->total_subkat_4}} </td>
+                                  <td>{{$datas->total_subkat_5}} </td>
+                                  <td>{{$datas->total_subkat_6}} </td>
+                                  <td>{{$datas->total_subkat_7}} </td>
+                                  <td>{{$datas->total_subkat_8}} </td>
+                                  <td>{{$datas->total_subkat_9}} </td>
+                                  <td>{{$datas->total_subkat_10}} </td>
+                                  <td>{{$datas->total_subkat_11}} </td>
+                                  <td>{{$datas->total_subkat_12}} </td>
+                                  <td>{{$datas->total_nilai_subkat}}</td>
+                              </tr>
+                                @php $semuanilai += $datas->total_nilai_subkat/3;
+                                  
+                                @endphp
+                              @endforeach    
+                              <tr>
+                                <th colspan="2">
+                                Total Nilai
+                                </th>
+                                <td colspan="15">
+                                @php 
+                                    echo($semuanilai);
+                                @endphp
+                                </td>
+                              </tr>                    
+                          </tbody>
+                          </table>
+                        
+                        <footer class="sticky-footer bg-white">
+                          <div class="container my-auto">
+                            <div class="copyright text-center my-auto">
+                              <span>Copyright &copy; PKK PRAJAB 2020</span>
+                            </div>
+                          </div>
+                        </footer>
+                        
 
-                </div>
+                
 
             </div>
 
-            <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; PKK PRAJAB 2020</span>
-          </div>
-        </div>
-      </footer>
-    </div>
+                
 </div>
  <!-- Bootstrap core JavaScript-->
  <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
